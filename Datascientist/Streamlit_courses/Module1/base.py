474e8cafa74d9cfa21e5e76998d48106f92dcc92
Abdoulaye FAYE <abdoulaye7.faye@uadb.edu.sn>
2025-01-08 02:13:14 +0000

    initiale commilt

---
+++ Datascientist/Streamlit_courses/Module1/base.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+dataframe = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
+
+st.dataframe(dataframe.style.highlight_max(axis=0))
+
+dataframe1 = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
+st.table(dataframe1)
+
+st.write("Affissage de graphies lineaires")
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
+
+st.write("Affichage de cartes")
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
+
+st.write()
